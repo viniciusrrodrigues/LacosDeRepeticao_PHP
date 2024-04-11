eb607e823e2dc5ef3cfbc57b1ff23afd5c213fd9
Vinicius Rafael <125403601+viniciusrrodrigues@users.noreply.github.com>
2024-04-11 15:23:11 -0300

    Add files via upload

---
+++ Aula_PW2_11_04/foreach.php
@@ -0,0 +1,10 @@
+<?php
+
+  //exemplo foreach
+
+  $arr = array(1, 2, 4);
+  foreach ($arr as $valor) {
+   echo $valor;
+  }
+
+  ?>
---
+++ Aula_PW2_11_04/vetor.php
@@ -0,0 +1,21 @@
+<?php
+$array = array(1,2,3);
+
+
+  for($i=0; $i<count($array); $i++){
+    echo $array[$i];
+  }
+
+// vetor
+
+  $a[0] = 10;
+  $a[1] = 20;
+  $a[2] = 30;
+  
+  
+// imprimindo os itens do vetor
+ 
+for($i=0; $i<count($a); $i++){
+    echo $a[$i];
+  }
+  ?>
